5a641fc2219552951115c0bcb3c3e04baa25d5ba
Jun Wen <jungel_wen@163.com>
2025-03-25 11:28:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 # abort entire script on error
 set -e
 
-module load gcc/6.2.0
-module load conda2/4.2.13
-conda create -n tf2 python=3.7.4
-source activate tf2
-pip install tensorflow==2.5.2
-pip install pandas
-pip install numpy
-pip install click
-pip install scipy
-pip install scikit-learn
-pip install configparser
+python3 -m venv pheMART_env 
+source pheMART_env/bin/activate 
+pip install -r requirements.txt
